025d02355931ec810c9b90e8ca65558f7572c680
YooInKeun <keun0390@naver.com>
2022-07-13 21:58:55 +0900

    chore: 개발 서버 배포 스크립트 추가

---
+++ deploy/deploy-dev.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+REPOSITORY_DIR=/home/ubuntu/app
+APP_NAME=semonemo-api
+PID_PATH=$REPOSITORY_DIR/PID
+
+echo "change directory"
+
+cd $REPOSITORY_DIR/$APP_NAME/web
+
+echo "git pull"
+
+git pull
+
+echo "app build"
+
+../gradlew clean build
+
+echo "copy executable jar file"
+
+cp $REPOSITORY_DIR/$APP_NAME/build/libs/*.jar $REPOSITORY_DIR
+
+echo "check current pid"
+
+if [ -f $PID_PATH ]; then
+	PID=$(cat $PID_PATH)
+	echo "stop current pid $PID"
+	kill -15 $PID
+	sleep 10
+fi
+
+echo "new application deploy"
+
+echo $(ls -tr $REPOSITORY_DIR | grep .jar | tail -n 1)
+
+JAR_NAME=$(ls -tr $REPOSITORY_DIR/ | grep .jar | tail -n 1)
+
+echo "jar name : $REPOSITORY_DIR/$JAR_NAME"
+
+nohup java -jar \
+  -Dspring.profiles.active=dev \
+   $REPOSITORY_DIR/$JAR_NAME 2>&1 & echo $! > $PID_PATH
+
