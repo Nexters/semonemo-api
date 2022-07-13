bcdbbb705202b91b611d3327033cea3015b3a093
YooInKeun <keun0390@naver.com>
2022-07-13 22:05:15 +0900

    chore: 헬쓰 체크 api 경로 변경

--- web/src/main/kotlin/semonemo/controller/HealthCheckController.kt
+++ web/src/main/kotlin/semonemo/controller/HealthCheckController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HealthCheckController {
 
-    @GetMapping("/health")
+    @GetMapping("/")
     fun healthCheck(): ResponseEntity<String> {
         return ResponseEntity.ok("\uD83C\uDF7A\uD83C\uDF7A\uD83C\uDF7A 맥주값만12마넌 \uD83C\uDF7A\uD83C\uDF7A\uD83C\uDF7A")
     }
