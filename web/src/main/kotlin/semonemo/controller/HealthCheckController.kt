b05e4abecd04ae4203da040d730056a67b658993
YooInKeun <keun0390@naver.com>
2022-07-13 20:34:21 +0900

    chore: health check api 추가

---
+++ web/src/main/kotlin/semonemo/controller/HealthCheckController.kt
@@ -0,0 +1,14 @@
+package semonemo.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+
+    @GetMapping("/health")
+    fun healthCheck(): ResponseEntity<String> {
+        return ResponseEntity.ok("\uD83C\uDF7A\uD83C\uDF7A\uD83C\uDF7A 맥주값만12마넌 \uD83C\uDF7A\uD83C\uDF7A\uD83C\uDF7A")
+    }
+}
