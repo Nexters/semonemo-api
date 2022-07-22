976412e481517cdcd28f4b34ed7eb76d327bf9eb
YooInKeun <keun0390@naver.com>
2022-07-22 17:47:08 +0900

    feat: 로그인 API, 기능 구현 추가

--- domain/src/main/kotlin/semonemo/model/entity/User.kt
+++ domain/src/main/kotlin/semonemo/model/entity/User.kt
@@ -2,16 +2,20 @@ package semonemo.model.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.io.Serializable
 
 @Document
 class User(
     nickname: String,
-) {
+): Serializable {
 
     @Id
     var id: Long? = null
         private set
 
     var nickname = nickname
+        private set
+
+    var authKey: String? = null
         private set
 }
