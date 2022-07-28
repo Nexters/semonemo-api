52b436c7549c723ebc90a5517bf7574e546c81b6
YooInKeun <keun0390@naver.com>
2022-07-28 17:27:07 +0900

    refactor: Repository, 기본키 타입 Long으로 변경

--- domain/src/main/kotlin/semonemo/repository/UserRepository.kt
+++ domain/src/main/kotlin/semonemo/repository/UserRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Mono
 import semonemo.model.entity.User
 
-interface UserRepository : ReactiveCrudRepository<User, String> {
+interface UserRepository : ReactiveCrudRepository<User, Long> {
 
     fun findUserByAuthKey(authKey: String): Mono<User>
 }
