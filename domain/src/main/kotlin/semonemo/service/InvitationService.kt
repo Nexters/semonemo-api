666c2cd57ead77315329fe339fcaf8be1378fac4
YooInKeun <keun0390@naver.com>
2022-08-03 18:47:49 +0900

    chore: 예외 발생 메세지 수정

--- domain/src/main/kotlin/semonemo/service/InvitationService.kt
+++ domain/src/main/kotlin/semonemo/service/InvitationService.kt
@@ -9,24 +9,22 @@ import semonemo.model.entity.User
 import semonemo.repository.CountersRepository
 import semonemo.repository.InvitationRepository
 import semonemo.repository.MeetingRepository
-import semonemo.repository.UserRepository
 
 @Service
 class InvitationService(
     private val invitationRepository: InvitationRepository,
     private val meetingRepository: MeetingRepository,
     private val countersRepository: CountersRepository,
-    private val userRepository: UserRepository,
 ) {
 
     // TODO: 리팩토링 필요. 웹플럭스 어려웡
     @Transactional
     fun saveInvitation(guest: User, request: InvitationSaveRequest): Mono<Invitation> =
         meetingRepository.findById(request.meetingId)
-            .switchIfEmpty(Mono.defer { Mono.error(IllegalArgumentException("존재하지 않는 모임입니다. (meetingId: ${request.meetingId})")) })
+            .switchIfEmpty(Mono.defer { Mono.error(IllegalArgumentException("존재하지 않는 모임입니다.")) })
             .flatMap { meeting ->
                 invitationRepository.findByMeetingIdAndUserId(meeting.id, guest.id!!)
-                    .flatMap<Invitation?> { Mono.defer { Mono.error(IllegalArgumentException("이미 초대된 모임입니다. (meetingId: ${request.meetingId}, user: ${guest.nickname})")) } }
+                    .flatMap<Invitation?> { Mono.defer { Mono.error(IllegalArgumentException("이미 초대된 모임입니다.")) } }
                     .switchIfEmpty(countersRepository.findById("invitationId")
                         .flatMap { counter ->
                             counter.increaseSeq()
