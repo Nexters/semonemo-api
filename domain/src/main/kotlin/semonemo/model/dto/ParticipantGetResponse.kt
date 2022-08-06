7b277d9de3998187510e41766f64e7ba0c3cd228
YooInKeun <keun0390@naver.com>
2022-08-06 16:47:25 +0900

    chore: 참가자 응답 포맷에 id 추가

--- domain/src/main/kotlin/semonemo/model/dto/ParticipantGetResponse.kt
+++ domain/src/main/kotlin/semonemo/model/dto/ParticipantGetResponse.kt
@@ -3,6 +3,7 @@ package semonemo.model.dto
 import semonemo.model.entity.User
 
 data class ParticipantGetResponse(
+    val id: Long,
     val nickname: String,
     val group: String?,
     // TODO: 출석 처리 기능 작업할 때, 처리 필요
@@ -12,6 +13,6 @@ data class ParticipantGetResponse(
     companion object {
         fun listOf(participants: List<User>): List<ParticipantGetResponse> = participants.map { of(it) }
 
-        private fun of(user: User) = ParticipantGetResponse(user.nickname, user.group)
+        private fun of(user: User) = ParticipantGetResponse(user.id!!, user.nickname, user.group)
     }
 }
